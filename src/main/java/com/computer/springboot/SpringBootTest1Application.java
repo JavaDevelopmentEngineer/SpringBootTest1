d0139cd2583514355d28f43f1340dedc0b83c948
admin <admin@gjy.com>
2022-06-15 14:36:45 +0800

    hot commit 14.36 two

--- src/main/java/com/computer/springboot/SpringBootTest1Application.java
+++ src/main/java/com/computer/springboot/SpringBootTest1Application.java
@@ -9,6 +9,7 @@ public class SpringBootTest1Application {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTest1Application.class, args);
         System.out.println("hot 第一次修改");
+        System.out.println("hot 第二次修改");
     }
 
 }

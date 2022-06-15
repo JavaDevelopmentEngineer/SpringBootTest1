eafae4ae8ad8ab81cfd7bb43c7e6d06006a1cb18
admin <admin@gjy.com>
2022-06-15 14:33:34 +0800

    ljt commit 14.33 three

--- src/main/java/com/computer/springboot/SpringBootTest1Application.java
+++ src/main/java/com/computer/springboot/SpringBootTest1Application.java
@@ -9,6 +9,7 @@ public class SpringBootTest1Application {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTest1Application.class, args);
         System.out.println("22222222");
+        System.out.println("33333333");
     }
 
 }
